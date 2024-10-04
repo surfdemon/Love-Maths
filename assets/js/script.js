9edfbd004a4a49554303dce83e67b644141109c4
Robert Beach <surfdemon@gmail.com>
2024-10-04 08:43:37 +0000

    Maint: Add functionality to display addition questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 alert("You clicked submit!");
             } else { 
                 let gameType = button.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame('addition');
 })
 
 
@@ -20,11 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
-const runGame = () => { 
+const runGame = (gameType) => { 
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 +1);
     let num2 = Math.floor(Math.random() * 25 +1);
 
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else { 
+        alert('unknown game type');
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 const checkAnswer = () => { }
@@ -35,7 +42,11 @@ const incrementScore = () => { }
 
 const incrementWrongScore = () => { }
 
-const displayAdditionQuestion = () => { }
+const displayAdditionQuestion = (operand1, operand2) => {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+ }
 
 const displaySubtractQuestion = () => { }
 
